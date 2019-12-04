1f56ca4e88164c94cbd0192bb9321e85c4155352
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-04 20:24:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,14 +12,14 @@ def third_challenge
          :name => "Romeo",
          :age => "15"
     },
-       :hero_friends => [{name:, age:, attitude:}, {name:, age:, attitude:}]#your key/value pairs here!
+       :hero_friends => [{name: , age: , attitude: }, {name: , age: , attitude: }]#your key/value pairs here!
 },
     :capulet => {
       :patriarch => { :name => "Lord Capulet",
       :age => "50"}, :matriarch => {:name => "Lady Capulet",
       :age => "51"}, :heroine => {:name => "Juliet",
       :age => "15",
-      :status => "alive"}, :heroine_friends => [{name:, age:, attitude:}, {name:, age:, attitude:}]#your key/value pairs here!
+      :status => "alive"}, :heroine_friends => [{name: , age: , attitude:}, {name: , age: , attitude: }]#your key/value pairs here!
       }
 
 
